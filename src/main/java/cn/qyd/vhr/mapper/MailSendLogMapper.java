99c184f464b781165dd049fb5373311d69f95220
qiuyunduo <qiuyunduo@aliyun.com>
2020-04-13 12:38:06 +0800

    v1.0

--- src/main/java/cn/qyd/vhr/mapper/MailSendLogMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.qyd.vhr.mapper;
-
-import cn.qyd.vhr.bean.MailSendLog;
-
-public interface MailSendLogMapper {
-    int insert(MailSendLog record);
-
-    int insertSelective(MailSendLog record);
-}
